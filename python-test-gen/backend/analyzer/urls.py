8cc93a30826896bd4d19c591c95207bbb2dfe22f
Kr0nys <144237820+Kr0nys@users.noreply.github.com>
2026-04-19 15:32:49 +0300

    Delete python-test-gen directory

--- python-test-gen/backend/analyzer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AnalysisSessionViewSet, TestGenerationTaskViewSet
-
-router = DefaultRouter()
-router.register(r'sessions', AnalysisSessionViewSet, basename='session')
-router.register(r'test-tasks', TestGenerationTaskViewSet, basename='test-task')
-
-urlpatterns = [path('', include(router.urls))]
